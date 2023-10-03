742e8a0df4aae8b129e61af53f8bb4068c116bc4
Rencist <dafianakhdan@gmail.com>
2023-10-03 14:58:54 +0700

    feat: innitial commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name', 128);
+            $table->string('email')->index();
+            $table->string('no_telp', 128);
+            $table->string('user_type', 128);
+            $table->integer('age');
+            $table->string('image_url', 128);
+            $table->string('password', 64);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
